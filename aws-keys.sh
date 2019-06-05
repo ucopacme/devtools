3ba0850a1f6c126296e54f92d3a683f6a26b0352
Eric Odell <eric.odell@ucop.edu>
2019-06-04 22:08:05 -0700

    box and goochrome

--- aws-keys.sh
+++ aws-keys.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cli_key=$(grep -v Access $i | awk -F , '{print $1}')
-cli_secret=$(grep -v Access $i | awk -F , '{print $2}')
+cli_key=$(grep -v Access $1 | awk -F , '{print $1}')
+cli_secret=$(grep -v Access $1 | awk -F , '{print $2}')
+echo export cli_key=$cli_key
+echo export cli_secret=$cli_secret
